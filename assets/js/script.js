6e18a7385728d1d90097d3fbc8fa24bc317a6724
Brandon N <brandonnelson2@gmail.com>
2024-04-29 21:49:23 +0100

    add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,11 +43,13 @@ function checkAnswer() {
   let isCorrect = userAnswer === calculatedAnswer[0];
 
   if (isCorrect) {
-    alert("Hey! You got it right!");
+    alert("Hey! You got it right! :D");
+    incrementScore();
   } else {
     alert(
       `Awww.... You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}.`
     );
+    incrementWrongAnswer();
   }
 }
 
@@ -68,9 +70,21 @@ function calculateCorrectAnswer() {
   }
 }
 
-function incrementScore() {}
+/**
+ * Gets the current score from the dom and increments it by 1
+ */
+function incrementScore() {
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = ++oldScore;
+}
 
-function incrementWrongAnswer() {}
+/**
+ * Gets the current tally of incorrect answers from the dom and increments it by 1
+ */
+function incrementWrongAnswer() {
+  let oldScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++oldScore;
+}
 
 function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
